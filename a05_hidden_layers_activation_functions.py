a7bfaf672d86d3169175dc96a773eb0bf1ae22a3
Ricardo <ricardobassanesi@gmail.com>
2022-10-17 15:49:05 -0300

    aula 5

--- a05_hidden_layers_activation_functions.py
+++ a05_hidden_layers_activation_functions.py
@@ -1,10 +1,12 @@
 import numpy as np
+import nnfs
+from nnfs.datasets import spiral_data 
+
+nnfs.init()
 
 np.random.seed(0)
 
-X = [[1, 2, 3, 2.5],
-     [2.0, 5.0, -1.0, 2.0],
-     [-1.5, 2.7, 3.3, -0.8]]
+X, y = spiral_data(100, 3)
 
 class Layer_Dense:
     
@@ -14,54 +16,18 @@ class Layer_Dense:
     
     def forward(self, inputs):
         self.output = np.dot(inputs, self.weights) + self.biases
+
+    
+class Activation_ReLU:
+    
+    def forward(self, inputs):
         
-layer1 = Layer_Dense(4, 5)
-layer2 = Layer_Dense(5, 2)
+        self.output = np.maximum(0, inputs)
+        
+layer1 = Layer_Dense(2, 5)
+activation1 = Activation_ReLU()
+
+
 
 layer1.forward(X)
-layer2.forward(layer1.output)
-    
-
-'''
-inputs = [[1, 2, 3, 2.5],
-          [2.0, 5.0, -1.0, 2.0],
-          [-1.5, 2.7, 3.3, -0.8]]
-
-weights = [[0.2, 0.8, -0.5, 1.0],
-           [0.5, -0.91, 0.26, -0.5],
-           [-0.26, -0.27, 0.17, 0.87]]
-
-biases = [2, 3, 0.5]
-
-weights2 = [[0.1, -0.14, 0.5],
-           [-0.5, 0.12, -0.33],
-           [-0.44, 0.73, -0.13]]
-
-biases2 = [-1, 2, -0.5]
-
-layer1_output = np.dot(inputs, np.array(weights).T) + biases
-layer2_output = np.dot(layer1_output, np.array(weights2).T) + biases2
-
-
-weights = [[0.2, 0.8, -0.5, 1.0],
-           [0.5, -0.91, 0.26, -0.5],
-           [-0.26, -0.27, 0.17, 0.87]]
-
-biases = [2, 3, 0.5]
-'''
-
-'''
-layers_outputs = []
-
-for neuron_weights, neuron_bias in zip(weights, biases):
-    
-    neuron_output = 0
-
-    for n_input, weight in zip(inputs, neuron_weights):
-        
-        neuron_output += n_input * weight
-
-    neuron_output += neuron_bias
-    layers_outputs.append(neuron_output)
-'''
-
+activation1.forward(layer1.output)
